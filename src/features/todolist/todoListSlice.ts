cba1f3abca8c37ee3ae9af036e47cbd1c43951c3
kanghunK <ltbehappy16@gmail.com>
2023-04-14 17:42:35 +0900

    Modify: extraReducers 동작에 따른 state 수정

--- src/features/todolist/todoListSlice.ts
+++ src/features/todolist/todoListSlice.ts
@@ -1,28 +1,45 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { addFetcher, deleteFetcher } from "./todoListApI";
 
+export interface addListAsyncParam {
+  date: string;
+  content: string;
+}
+
+export interface Content {
+  order: number;
+  date: string;
+  content: string;
+}
+
 export interface todoListState {
   listNumber: number;
-  addState: string;
-  editState: string;
-  deleteState: string;
+  dataList: Content[];
+  workType: string;
+  workState: string;
 }
 
 const initialState: todoListState = {
   listNumber: 0,
-  addState: "idle",
-  editState: "idle",
-  deleteState: "idle",
+  dataList: [],
+  workType: "", // add, edit, delete
+  workState: "idle",
 };
 
-export const addListAsync = createAsyncThunk(
-  "todoList/addList",
-  async () => await addFetcher()
-);
+export const addListAsync = createAsyncThunk<
+  addListAsyncParam,
+  addListAsyncParam
+>("todoList/addList", async (dataObj) => {
+  await addFetcher();
+  return dataObj;
+});
 
-export const deleteListAsync = createAsyncThunk(
+export const deleteListAsync = createAsyncThunk<number, number>(
   "todoList/deleteList",
-  async () => await deleteFetcher()
+  async (order) => {
+    await deleteFetcher();
+    return order;
+  }
 );
 
 export const todoListSlice = createSlice({
@@ -31,34 +48,58 @@ export const todoListSlice = createSlice({
   reducers: {
     addList: (state) => {
       state.listNumber += 1;
+      state.workState = "idle";
     },
     deleteList: (state) => {
       state.listNumber -= 1;
+      state.workState = "idle";
     },
   },
   extraReducers: (builder) => {
     builder
       .addCase(addListAsync.pending, (state) => {
-        state.addState = "loading";
+        state.workType = "add";
+        state.workState = "loading";
       })
-      .addCase(addListAsync.fulfilled, (state) => {
-        state.addState = "idle";
+      .addCase(addListAsync.fulfilled, (state, actions) => {
+        state.workType = "idle";
+        state.workState = "success";
+
         state.listNumber += 1;
+
+        const newContent = { ...actions.payload, order: state.listNumber };
+        state.dataList = [...state.dataList, newContent];
       })
       .addCase(addListAsync.rejected, (state) => {
-        state.addState = "faild";
+        state.workType = "idle";
+        state.workState = "faild";
       })
       .addCase(deleteListAsync.pending, (state) => {
-        state.addState = "loading";
+        state.workType = "delete";
+        state.workState = "loading";
       })
-      .addCase(deleteListAsync.fulfilled, (state) => {
-        state.addState = "idle";
+      .addCase(deleteListAsync.fulfilled, (state, actions) => {
+        state.workType = "idle";
+        state.workState = "success";
         state.listNumber -= 1;
+
+        const filterList = state.dataList.filter(
+          (data) => data.order !== actions.payload
+        );
+        state.dataList = filterList.map((data, i) => {
+          return {
+            ...data,
+            order: i,
+          };
+        });
       })
       .addCase(deleteListAsync.rejected, (state) => {
-        state.addState = "faild";
+        state.workType = "idle";
+        state.workState = "faild";
       });
   },
 });
+
+export const { addList, deleteList } = todoListSlice.actions;
 
 export default todoListSlice.reducer;
